842b98dd6b07f9aa6af26996489b9c24e25f55c2
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-08-24 03:45:46 -0700

    Be super clever

--- lua/safespace/utils.lua
+++ lua/safespace/utils.lua
@@ -1,12 +1,38 @@
-SafeSpace = {}
+local rawget = rawget
+local rawset = rawset
+local unpack = unpack
+local TEXT_FILTER_UNKNOWN = TEXT_FILTER_UNKNOWN
+local FilterText = util.FilterText
 
-local filter = function( text )
-    return util.FilterText( text, TEXT_FILTER_UNKNOWN )
+-- TODO: This needs to be smartly cleaned regularly so it doesn't get really, really big
+SafeSpace = { Cache = {} }
+local cache = SafeSpace.Cache
+
+local _filter = function( text )
+    return FilterText( text, TEXT_FILTER_UNKNOWN )
+end
+
+local function filter( text )
+    if not text then return end
+
+    local cached = rawget( cache, text )
+    if cached then
+        return cached == true and text or cached
+    end
+
+    -- We just save "true" if the
+    -- text remained unchanged to save tons of memory
+    local result = _filter( text )
+    local toSave = result == text and true or result
+    rawset( cache, text, toSave )
+
+    return result
 end
 
 local isEnabled = CreateClientConVar( "safespace_enabled", 1, true, false, "Enable/Disable SafeSpace", 0, 1 ):GetBool()
 cvars.AddChangeCallback( "safespace_enabled", function( _, _, new )
     isEnabled = tobool( new )
+    SafeSpace.Cache = {}
 end, "UpdateLocalValue" )
 
 SafeSpace.wrap = function( lib, key, textIndex, argHandler )
@@ -26,9 +52,15 @@ SafeSpace.wrap = function( lib, key, textIndex, argHandler )
         if argHandler then
             args = argHandler( args, filter )
         else
-            args[textIndex] = filter( args[textIndex] )
+            -- args[textIndex] = filter( args[textIndex] )
+            rawset( args, textIndex, filter( rawget( args, textIndex ) ) )
         end
 
         return og( unpack( args ) )
     end
 end
+
+hook.Add( "Think", "SafeSpace_CacheFilterText", function()
+    hook.Remove( "Think", "SafeSpace_CacheFilterText" )
+    FilterText = util.FilterText
+end )
